bebaea0f8f1a690e1cb053137fdad63c02d2eed4
renyh <renyh1013@163.com>
2017-07-05 04:17:47 +0800

    用TCPClient发送接收消息

--- dp2SIPClient/MainForm.Designer.cs
+++ dp2SIPClient/MainForm.Designer.cs
@@ -28,20 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox_printer = new System.Windows.Forms.ListBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 114);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 24);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "发送消息：";
+            // 
+            // listBox_printer
+            // 
+            this.listBox_printer.FormattingEnabled = true;
+            this.listBox_printer.ItemHeight = 24;
+            this.listBox_printer.Location = new System.Drawing.Point(15, 180);
+            this.listBox_printer.Margin = new System.Windows.Forms.Padding(6);
+            this.listBox_printer.Name = "listBox_printer";
+            this.listBox_printer.Size = new System.Drawing.Size(1086, 556);
+            this.listBox_printer.TabIndex = 19;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(181, 108);
+            this.txtMsg.Margin = new System.Windows.Forms.Padding(6);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(578, 35);
+            this.txtMsg.TabIndex = 18;
+            this.txtMsg.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtMsg_KeyUp_1);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(793, 104);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(6);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(150, 46);
+            this.btnSend.TabIndex = 17;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(955, 16);
+            this.btnDown.Margin = new System.Windows.Forms.Padding(6);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(150, 46);
+            this.btnDown.TabIndex = 16;
+            this.btnDown.Text = "停止";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(793, 16);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(6);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(150, 46);
+            this.btnConnect.TabIndex = 15;
+            this.btnConnect.Text = "连接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(469, 26);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "端口：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 24);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "服务器地址：";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(563, 20);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(6);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(196, 35);
+            this.txtPort.TabIndex = 12;
+            this.txtPort.Text = "2000";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(181, 20);
+            this.txtIP.Margin = new System.Windows.Forms.Padding(6);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(272, 35);
+            this.txtIP.TabIndex = 11;
+            this.txtIP.Text = "127.0.0.1";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(869, 532);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(1149, 767);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox_printer);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox_printer;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtIP;
     }
 }
 
--- dp2SIPClient/MainForm.cs
+++ dp2SIPClient/MainForm.cs
@@ -3,17 +3,222 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace dp2SIPClient
 {
     public partial class MainForm : Form
     {
+        private TcpClient _client;
+
+        private NetworkStream _networkStream;
+        private StreamReader _streamReader;
+        private StreamWriter _streamWriter;
+
+        private Thread _recvThread;   // 接收信息线程
+        //private Thread _sendThread;   // 发送信息线程
+        private Thread _serverThread;// 服务线程
+
         public MainForm()
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Connection()
+        {
+            try
+            {
+                IPAddress ipAddress = IPAddress.Parse(txtIP.Text);
+                Int32 port = Int32.Parse(txtPort.Text);
+                string hostName = Dns.GetHostEntry(ipAddress).HostName;
+                _client = new TcpClient(hostName, port);
+            }
+            catch
+            {
+                MessageBox.Show("没有连接到服务器!");
+                return;
+            }
+
+            this.SetText("客户端成功连接上服务器");
+
+            SetCtrlState(false);
+
+            _networkStream = _client.GetStream();
+            _streamReader = new StreamReader(_networkStream);
+            _streamWriter = new StreamWriter(_networkStream);
+
+            // 创建接收信息线程，并启动
+            _recvThread = new Thread(new ThreadStart(RecvData));
+            _recvThread.Start();
+        }
+
+        // 接收数据
+        private void RecvData()
+        {
+            string s = _streamReader.ReadLine();
+            if (s == null)
+                return;
+
+            // 如果没接到服务器退出的消息，则继续接收信息
+            while (s !=null && !s.Equals("exit"))
+            {
+                this.SetText("收到信息:" + s);
+                s = _streamReader.ReadLine();
+            }
+
+
+            // 收到服务器退出的消息，
+            this.SetText("服务器关闭");
+            this.SetText("客户端关闭");
+
+            // 设置按钮状态
+            this.SetCtrlState(true);
+
+            //释放资源
+            ReleaseResouce();
+        }
+
+        // 释放资源
+        private void ReleaseResouce()
+        {
+            _networkStream.Close();
+            _streamReader.Close();
+            _streamWriter.Close();
+
+            //_sendThread.Abort();
+            _recvThread.Abort();
+            _serverThread.Abort();
+
+            _client.Close();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            _serverThread = new Thread(new ThreadStart(Connection));
+            _serverThread.Start();
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            this.SetCtrlState(true);
+            //this.btnConnect.Enabled = true;  // 按了停止之后，“连接”按钮可以用，“发送”不能用
+            //this.btnDown.Enabled = false;
+            //this.btnSend.Enabled = false;
+
+
+            string exitMsg = "exit";  // 要退出时，发送 exit 信息给服务器
+            _streamWriter.WriteLine(exitMsg);
+            //刷新当前数据流中的数据
+            _streamWriter.Flush();
+
+            // 刷新界面
+            this.SetText("客户端关闭");
+
+            //释放资源
+            ReleaseResouce();
+        }
+        delegate void SetTextCallback(string text);
+
+        delegate void SetCtrlStateCallback(bool enabled);
+        private void SetCtrlState(bool enabled)
+        {
+            if (this.btnConnect.InvokeRequired)
+            {
+                SetCtrlStateCallback d = new SetCtrlStateCallback(SetCtrlState);
+                this.Invoke(d, new object[] { enabled });
+            }
+            else
+            {
+                btnConnect.Enabled = enabled;
+                btnDown.Enabled = !enabled;
+                btnSend.Enabled = !enabled;
+            }
+        }
+
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.listBox_printer.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.listBox_printer.Items.Add(text);
+               // this.textBox1.Text = text;
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            //// 启动发送线程
+            //_sendThread = new Thread(new ThreadStart(SendData));
+            //_sendThread.Start();
+
+            SendData();
+        }
+
+        // 发送数据
+        private void SendData()
+        {
+            // 发送信息不允许为空
+            if (txtMsg.Text == "")
+            {
+                MessageBox.Show("发送的信息不能为空!");
+                txtMsg.Focus();
+                return;
+            }
+
+            try
+            {
+                //往当前的数据流中写入一行字符串
+                _streamWriter.WriteLine(txtMsg.Text);
+                
+                //刷新当前数据流中的数据
+                _streamWriter.Flush();
+
+                // 刷新界面
+                this.SetText("发送信息:" + txtMsg.Text);
+                txtMsg.Text = "";  // 清空
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Client提示");
+            }
+        }
+
+        private void txtMsg_Enter(object sender, EventArgs e)
+        {
+            this.SendData();
+        }
+
+        private void txtMsg_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void txtMsg_KeyUp_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.SendData();
+            }
+        }
+
     }
 }
